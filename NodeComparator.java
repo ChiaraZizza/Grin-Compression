1b17136b1a717fb299491aa6568f9532a016e9fa
Chiara Zizza <huachucat@gmail.com>
2017-01-04 11:31:16 -0700

    Update NodeComparator.java

--- NodeComparator.java
+++ NodeComparator.java
@@ -3,10 +3,6 @@ import java.util.Comparator;
 /** Explains how to compare two Nodes
  * @author chiarazizza
  */
-
-//http://stackoverflow.com/questions/683041/java-how-do-i-use-a-priorityqueue
-//https://www.tutorialspoint.com/java/java_using_comparator.htm
-//http://algs4.cs.princeton.edu/55compression/Huffman.java.html
 public class NodeComparator implements Comparator<Node> {
 		@Override
 		public int compare(Node n1, Node n2) {
